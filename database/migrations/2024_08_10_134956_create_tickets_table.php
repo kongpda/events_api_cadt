ed58190631b85b2a415aae0a410fd67454981592
kongpda <kongpda@gmail.com>
2025-02-09 21:49:05 +0700

    refactor(database): update tickets table schema
    Change the tickets table schema to use ULID for primary and foreign 
    keys, and add new fields for status, purchase date, and price. This 
    improves uniqueness and scalability while enhancing the data model 
    to better track ticket information.

--- database/migrations/2024_08_10_134956_create_tickets_table.php
+++ database/migrations/2024_08_10_134956_create_tickets_table.php
@@ -6,18 +6,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class () extends Migration {
+return new class() extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('tickets', function (Blueprint $table): void {
-            $table->id();
-            $table->foreignId('event_id')->constrained()->onDelete('cascade');
-            $table->foreignId('order_id')->nullable()->constrained()->onDelete('set null');
-            $table->string('ticket_number')->unique();
-            $table->boolean('is_used')->default(false);
+            $table->ulid('id')->primary();
+            $table->foreignUlid('event_id')->constrained()->cascadeOnDelete();
+            $table->foreignUlid('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignUlid('ticket_type_id')->constrained()->cascadeOnDelete();
+
+            $table->string('status');
+            $table->timestamp('purchase_date');
+            $table->decimal('price', 10, 2);
             $table->timestamps();
         });
     }
