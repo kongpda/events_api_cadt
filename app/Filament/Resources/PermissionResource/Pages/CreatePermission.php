18fe554cbf8e54e7b3ac4b6316a909d9d534bad6
kongpda <kongpda@gmail.com>
2025-02-12 00:49:44 +0700

    feat: enhance permission resource pages with strict types
    Add strict types declaration to `EditPermission` and `CreatePermission` 
    classes. Mark classes as final to prevent inheritance. Implement 
    `getRedirectUrl` method in both classes to ensure consistent redirection 
    to the index page after actions. These changes improve type safety and 
    maintainability of the code.

--- app/Filament/Resources/PermissionResource/Pages/CreatePermission.php
+++ app/Filament/Resources/PermissionResource/Pages/CreatePermission.php
@@ -1,12 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Filament\Resources\PermissionResource\Pages;
 
 use App\Filament\Resources\PermissionResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
-class CreatePermission extends CreateRecord
+final class CreatePermission extends CreateRecord
 {
     protected static string $resource = PermissionResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
--- app/Filament/Resources/PermissionResource/Pages/EditPermission.php
+++ app/Filament/Resources/PermissionResource/Pages/EditPermission.php
@@ -1,14 +1,21 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Filament\Resources\PermissionResource\Pages;
 
 use App\Filament\Resources\PermissionResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 
-class EditPermission extends EditRecord
+final class EditPermission extends EditRecord
 {
     protected static string $resource = PermissionResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 
     protected function getHeaderActions(): array
     {
