788cc50d997f8aae6c13c9081b5fb7c32e3b6677
kongpda <kongpda@gmail.com>
2025-02-11 13:32:57 +0700

    (wip)

--- app/Http/Resources/EventParticipantResource.php
+++ app/Http/Resources/EventParticipantResource.php
@@ -12,20 +12,26 @@ final class EventParticipantResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'type' => 'event_participants',
             'id' => $this->id,
-            'event_id' => $this->event_id,
-            'user_id' => $this->user_id,
-            'status' => $this->status->value,
-            'participation_type' => $this->participation_type->value,
-            'ticket_id' => $this->ticket_id,
-            'check_in_time' => $this->check_in_time,
-            'joined_at' => $this->joined_at,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
-            // Related resources
-            'event' => new EventResource($this->whenLoaded('event')),
-            'user' => new UserResource($this->whenLoaded('user')),
-            'ticket' => new TicketResource($this->whenLoaded('ticket')),
+            'attributes' => [
+                'event_id' => $this->event_id,
+                'user_id' => $this->user_id,
+                'status' => $this->status->value,
+                'participation_type' => $this->participation_type->value,
+                'ticket_id' => $this->ticket_id,
+                'check_in_time' => $this->check_in_time,
+                'joined_at' => $this->joined_at,
+                'created_at' => $this->created_at,
+                'updated_at' => $this->updated_at,
+            ],
+            'relationship' => [
+                $this->mergeWhen($request->routeIs('event_participants.show'), [
+                    'event' => new EventResource($this->whenLoaded('event')),
+                    'user' => new UserResource($this->whenLoaded('user')),
+                    'ticket' => new TicketResource($this->whenLoaded('ticket')),
+                ]),
+            ],
         ];
     }
 }
